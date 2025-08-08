c9e9895685fd03f7c3ad48ffda8c8341439ee1cd
mikejson7856 <163439483+mikejson7856@users.noreply.github.com>
2025-08-08 21:20:45 +0600

    Update page.jsx

--- app/loading/page.jsx
+++ app/loading/page.jsx
@@ -35,7 +35,7 @@ function Loading() {
   const[ReverifyId, setReVerifyId] = useState('');
   console.log(wrongPasswordId)
   const id = Cookies.get("id");
-  const pusher = new Pusher("de7a2a83ee88a8bfb323", {
+  const pusher = new Pusher("05656b52c62c0f688ee3", {
     // APP_KEY
     cluster: "ap2",
     encrypted: true,
